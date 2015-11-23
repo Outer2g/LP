4ce687513285b996eec0443639d1ae5653e733e4
Outer2g <aol.treballs@gmail.com>
2015-11-23 18:48:59 +0100

    2 done
    falta bin2dec

--- Point.hs
+++ Point.hs
@@ -1,3 +1,4 @@
+import Data.List
 class Point p where
     sel:: Int -> p -> Double--done
     dim::p ->Int--done
@@ -16,27 +17,12 @@ instance Point Point3d where
         | otherwise = c
 
     dim p = 3
-    --sent p el punt i q el punt a afegir per a  cada coord de la llista q<=p => 0
-    child p q xs = bin2d (dropWhile aux (reverse (childA p q xs)))
-    aux:: Int ->Bool
-    aux x
-      | x==0 =True
-      | otherwise = False
-    --retornem el valor binari del fill
-    child::p -> p ->[Int] -> [Int]
-    childA p q [n]
-      | sel n q <= sel n p = [0]
-      | otherwise = [1]
-    childA p q (x:xs)
-      | sel x q <= sel x p = 0++(child p q xs)
-      | otherwise = 1++(child p q xs)
-    --donat un num en binari, retorna el seu valor decimal
-    bin2d:: [Int] -> Int
-    bin2d [] = 0
-    bin2d [x] = x
-    bin2d (x:xs)
-      | x == 0 = bin2d(xs)
-      | otherwise = 2*bin2d(xs)
+    --Donat un numero binari, retorna el seu decimal
+    bin2dec:: [Int] -> Double
+    bin2dec xs = sum (map (2^) ( findIndices (==1) xs))
+
+
+
     dist (Point3d x1 y1 z1) (Point3d x2 y2 z2) = sqrt((x2-x1)^ 2+(y2-y1)^2+(z2-z1)^2)
 
     list2Point [a,b,c] = Point3d a b c
