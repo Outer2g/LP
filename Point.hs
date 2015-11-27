c72b214866083a2b5f8590447bbf8b35afdbc105
Outer2g <aol.treballs@gmail.com>
2015-11-27 20:22:38 +0100

    Insert
    Insert done, not tested need Show

--- Point.hs
+++ Point.hs
@@ -69,6 +69,52 @@ instance (Show p,Point p) => Show (Kd2nTree p) where
       impFills (Node a fill ((Node x f ys):xs)) = "<"++show (child a x fill)++">"++(impNode (Node x f ys))++"\n"
                                                     ++(show (impFills (Node x f ys)))++
                                                     (show (impFills(Node a fill xs)))
+--Exercici 4
+insertt::(Point p,Eq p)=> Kd2nTree p -> p -> [Int] -> Kd2nTree p
+insertt Empty p fill = (Node p fill [])
+insertt (Node a fill []) p xs = (Node a fill (fillIt nen p xs))
+    where
+        nen = child a p fill
+insertt (Node a fill llista) p xs
+    | length llista >= nen && (llista!!nen) == Empty = (Node a fill (insertAt nen p xs llista))
+    | length llista >= nen && llista!!nen /= Empty = insertt (llista!!nen) p xs
+    | otherwise = (Node a fill magia)
+    where
+        nen = child a p fill
+        magia = (llista++(fillIt (length llista - nen) p xs))
+
+fillIt:: Int -> p -> [Int] -> [Kd2nTree p]
+fillIt pos p fill = fillEmpty (pos-1)++[(Node p fill [])]
+    where
+        fillEmpty:: Int -> [Kd2nTree p]
+        fillEmpty pos
+            | pos ==0 = [Empty]
+            | otherwise = [Empty]++fillEmpty (pos-1)
+
+insertAt:: Int -> p -> [Int] ->[Kd2nTree p] -> [Kd2nTree p]
+insertAt pos p fill (x:xs)
+    | length xs == pos = (Node p fill []):xs
+    | otherwise = insertAt pos p fill xs
+
+{-insert:: Kd2nTree p -> p -> [Int] -> Kd2nTree p
+insert Empty p fill = (Node p fill [])
+insert (Node a fill []) p xs =(Node a fill (itWillFit [] (child a p fill) p xs))
+insert (Node a fill list) p xs
+    | ((length list) >= nen) && ((list!!nen)== Empty)=(Node a fill (insertAt nen list))
+    | ((length list) >= nen) && ((list!!nen)\= Empty)=insert (list!!nen) p xs
+    | otherwise = (Node a fill (fitThat list p nen nen xs))
+    where nen = child a p fill
+fitThat:: [Kd2nTree p] -> p -> Int -> -> [Kd2nTree p]
+fitThat list p pos aux fill
+    | aux==pos = (fitThat list p pos (aux-1) fill)++[(Node p fill [])]
+    | length list < aux
+
+itWillFit::[Kd2nTree p] -> Int->p ->[Int]->[Kd2nTree p]
+itWillFit [] pos p fill
+    | pos == 0 = [(Node a fill [])]
+    | otherwise = [Empty] ++ itWillFit [] (pos-1) p fill
+
+-}
 --Exercici 5
 get_all:: Point p =>Kd2nTree p -> [(p,[Int])]
 get_all Empty = []
@@ -84,7 +130,20 @@ contains (Node a fill []) b = a==b
 contains (Node a fill (x:xs)) b 
   | b == a = True
   | otherwise = contains (Node a fill xs) b || contains x b
-
+--Exercici 8
+{-}nearest:: Kd2nTree p -> p -> p
+nearest (Node a fill list) q = fst myListMax ([a,dist a q]++getMax list q)
+    where
+        getMax::[kd2nTree p] -> p ->[(p,Double)]
+        getMax ((Node a fill []):[]) q = [a,(dist a q)]
+        getMax ((Node a fill xs):[]) q = [a,(dist a q)] ++ getMax xs q
+        getMax (Node a fill (x:xs)) q = [a,(dist a q)] ++ getMax (x:xs) q
+        myListMax::[(p,Double)] -> (p,Double)
+        myListMax (x:[]) = x
+        myListMax (x:xs)
+            |fst x > myListMax xs = x
+            | otherwise = myListMax xs
+-}
 --Exercici 9
 allinInterval:: Ord p =>Kd2nTree p -> p -> p -> [p]
 allinInterval (Node a fill []) b c
